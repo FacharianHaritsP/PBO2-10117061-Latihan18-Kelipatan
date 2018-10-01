b8cb430e6e58ca002f8bb9525db0f897f8ceba41
FacharianHaritsP <43494603+FacharianHaritsP@users.noreply.github.com>
2018-10-01 15:06:55 +0700

    Initial commit

---
+++ src/pbo2/pkg10117061/latihan18/kelipatan/PBO210117061Latihan18Kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117061.latihan18.kelipatan;
+
+/**
+ *
+ * @author Notebook
+ */
+public class PBO210117061Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        for (double Bil = 1; Bil <= 10; Bil++) {
+            System.out.println(Bil*3.5);
+            
+        }
+    }
+
+}
